0edd224b576cbdf68a6fe688fb4cdc2902b2c33d
Mateusz-11 <tweak.night@gmail.com>
2022-11-20 21:46:21 +0100

    Added views

--- project/showtimes/views.py
+++ project/showtimes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from showtimes.models import Cinema
+from showtimes.serializers import CinemaSerializer
 
-# Create your views here.
+
+class CinemaListView(generics.ListCreateAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class CinemaView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
