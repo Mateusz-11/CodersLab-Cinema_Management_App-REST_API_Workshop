311b55f5699e7c38c3263657b445b1203ddec943
Mateusz-11 <tweak.night@gmail.com>
2022-11-22 21:11:02 +0100

    Added tests

--- project/showtimes/tests/utils.py
+++ project/showtimes/tests/utils.py
@@ -1,9 +1,18 @@
 from random import sample
 from movielist.models import Movie
-
+from faker import Faker
+from showtimes.models import Screening
+import pytz
+faker = Faker("pl_PL")
+TZ = pytz.timezone(TIME_ZONE)
 
 def random_movies():
     """Return 3 random Movies from db."""
     movies = list(Movie.objects.all())
     return sample(movies, 3)
 
+def add_screenings(cinema):
+    """Add 3 screenings for given cinema."""
+    movies = random_movies()
+    for movie in movies:
+        Screening.objects.create(cinema=cinema, movie=movie, date=faker.date_time(tzinfo=TZ))
